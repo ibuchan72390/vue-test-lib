525f0b1c3e5fdd9de0e62dbc6898ce706a4f6349
Ian Buchan <ibuchan@prizelogic.com>
2019-01-04 15:14:20 -0700

    Test message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
+
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
@@ -15,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue';
   },
 })
 export default class App extends Vue {}
+
 </script>
 
 <style>
